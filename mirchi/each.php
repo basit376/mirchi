9cd37bbb6028eeda8171f796ab614416dfd5b32b
Ali Abdul Basit <basitali856@outlook.com>
2021-10-30 22:53:47 +0500

    added new files

--- mirchi/each.php
+++ mirchi/each.php
@@ -1,7 +1,10 @@
 <?php
 $prices = array('Chicken Roll'=>100, 'Tikka Boti'=>240, 'Mutton Handi'=>350, 'Pasta'=>120, 'Cuppuccino Coffe'=>150);
 
+echo "<ol>";
 foreach($prices as $key => $value){
-    echo $key."_".$value."<br />";
+    echo "<li> $key _$value</li>";
+    
 }
+echo "</ol>";
 ?>
